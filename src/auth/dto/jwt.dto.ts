c54860f11ee38b37898edcf5a0325bef40b3d91b
Nischal Shakya <nischalshakya2015@gmail.com>
2022-06-20 19:34:55 +0545

    Add jwt validate

---
+++ src/auth/dto/jwt.dto.ts
@@ -0,0 +1,6 @@
+export class JwtDto {
+  email: string;
+  sub: number;
+  iat: number;
+  exp: number;
+}
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { JwtDto } from '../dto/jwt.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+  constructor(private auth: AuthService, config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get<string>('JWT_SECRET'),
     });
+  }
+
+  validate(payload: JwtDto) {
+    return payload;
   }
 }
